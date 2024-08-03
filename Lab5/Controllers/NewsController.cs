98e67575238df98a74ef288f6e2696f90a9ff58a
Yijia Xu <yijiaxu.yjx@gmail.com>
2024-08-03 19:13:49 -0400

    The website can route to News Pages; NewsController generated

--- Lab5/Controllers/NewsController.cs
+++ Lab5/Controllers/NewsController.cs
@@ -1,43 +1,164 @@
-﻿using Azure.Storage.Blobs;
-using Lab5.Data;
-using Lab5.Models.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Lab5.Data;
+using Lab5.Models;
 
 namespace Lab5.Controllers
 {
     public class NewsController : Controller
     {
         private readonly SportsDbContext _context;
-        private readonly BlobServiceClient _blobServiceClient;
 
-        public NewsController(SportsDbContext context, BlobServiceClient blobServiceClient)
+        public NewsController(SportsDbContext context)
         {
             _context = context;
-            _blobServiceClient = blobServiceClient;
         }
 
-        public async Task<IActionResult> Index(string id)
+        // GET: News
+        public async Task<IActionResult> Index()
         {
-            var sportClub = await _context.SportClubs
-                .Include(sc => sc.News)
-                .FirstOrDefaultAsync(sc => sc.Id == id);
+            var sportsDbContext = _context.News.Include(n => n.SportClub);
+            return View(await sportsDbContext.ToListAsync());
+        }
 
-            if (sportClub == null)
+        // GET: News/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var viewModel = new NewsViewModel
+            var news = await _context.News
+                .Include(n => n.SportClub)
+                .FirstOrDefaultAsync(m => m.NewsId == id);
+            if (news == null)
             {
-                SportClub = sportClub,
-                News = sportClub.News
-            };
+                return NotFound();
+            }
 
-            return View(viewModel);
+            return View(news);
         }
 
+        // GET: News/Create
+        public IActionResult Create()
+        {
+            ViewData["SportClubId"] = new SelectList(_context.SportClubs, "Id", "Id");
+            return View();
+        }
+
+        // POST: News/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NewsId,FileName,Question,Url,SportClubId")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(news);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SportClubId"] = new SelectList(_context.SportClubs, "Id", "Id", news.SportClubId);
+            return View(news);
+        }
+
+        // GET: News/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News.FindAsync(id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+            ViewData["SportClubId"] = new SelectList(_context.SportClubs, "Id", "Id", news.SportClubId);
+            return View(news);
+        }
+
+        // POST: News/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("NewsId,FileName,Question,Url,SportClubId")] News news)
+        {
+            if (id != news.NewsId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(news);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NewsExists(news.NewsId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SportClubId"] = new SelectList(_context.SportClubs, "Id", "Id", news.SportClubId);
+            return View(news);
+        }
+
+        // GET: News/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .Include(n => n.SportClub)
+                .FirstOrDefaultAsync(m => m.NewsId == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var news = await _context.News.FindAsync(id);
+            if (news != null)
+            {
+                _context.News.Remove(news);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NewsExists(int id)
+        {
+            return _context.News.Any(e => e.NewsId == id);
+        }
     }
-
-
 }
